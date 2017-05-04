617c20794f1f161bb3d943d42812746d66d554b0
nobita <nobitacity2015@gmail.com>
2017-05-04 17:28:36 +0800

    app update to cylon version

--- app.js
+++ app.js
@@ -1,38 +1,15 @@
-console.log('WWW blink start ...');
+var Cylon = require('cylon');
 
-var ledPin = 13;
-var firmata = require('firmata');
+Cylon.robot({
+    connections: {
+        arduino: { adaptor: 'firmata', port: '/dev/ttyS0' }
+    },
 
-var board = new firmata.Board("/dev/ttyS0", function(err) {
-    if (err) {
-        console.log(err);
-        board.reset();
-        return;
+    devices: {
+        led: { driver: 'led', pin: 13 }
+    },
+
+    work: function(my) {
+        every((1).second(), my.led.toggle);
     }
-
-    console.log('connected...');
-    console.log('board.firmware: ', board.firmware);
-
-    board.pinMode(ledPin, board.MODES.OUTPUT);
-
-    var url = require('url');
-    var http = require('http');
-
-    http.createServer(function(request, response) {
-        var params = url.parse(request.url, true).query;
-        try {
-            if (params.value.toLowerCase() == 'high') {
-                board.digitalWrite(ledPin, board.HIGH);
-            } else if (params.value.toLowerCase() == 'low'){
-                board.digitalWrite(ledPin, board.LOW);
-            }
-        } catch(e) {
-
-        }
-        response.writeHead(200);
-        response.write("The value written was: " + params.value);
-        response.end();
-    }.bind(this)).listen(8080);
-
-    console.log('Listening on port 8080 ...');
-});
+}).start();
